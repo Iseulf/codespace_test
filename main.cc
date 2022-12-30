cf3386cf90d62041855c088c7783ce7af3f3ddf3
Iseulf <62506853+Iseulf@users.noreply.github.com>
2022-12-30 08:53:10 +0000

    test

--- main.cc
+++ main.cc
@@ -1,9 +1,13 @@
-#include <stdio.h>
+#include <iostream>
+#include <map>
+#include <string>
 #include <unordered_map>
+#include <vector>
 using namespace std;
-int main()
-{
-    unordered_map<int, int> a;
-    printf("Hello\n");
+
+int main() {
+    unordered_map<string, vector<string>> mp;
+    mp["seulf"] = {"12", "23"};
+    cout << mp["seulf"][0] << endl;
     return 0;
 }
