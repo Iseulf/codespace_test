eb217318386e1a3cab27c637d3f060bc3af54ac0
Iseulf <62506853+Iseulf@users.noreply.github.com>
2023-01-03 09:29:31 +0000

    日常提交

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char const *argv[]) {
     auto mp = read_json_data("./data/data-json.json");
     // auto vp = find_entitys(mp, "国籍", [](string x) { return x == "中国"; });
     cout << find_attribute(mp, "邓子恢", "陈兰") << endl;
-    cout << find_attribute_value(mp, "邓子恢", "妻子") << endl;
+    cout << find_attribute_value(mp, "钱伟长", "妻子") << endl;
     // for (auto s : vp)
     // cout << s << endl;
     return 0;
